5d412d8f3c1533670d15567860468d981a0a09f3
Jas <jason.gerfen@gmail.com>
2017-11-19 01:25:51 -0700

    Added generated STIG modules for additional UNIX/Linux OS(s)

---
+++ tools/stigs/HP-UX/11.31/V00761.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+# Global defaults for tool
+author=
+verbose=0
+change=0
+meta=0
+restore=0
+interactive=0
+
+# Working directory
+cwd="$(dirname $0)"
+
+# Tool name
+prog="$(basename $0)"
+
+
+# Copy ${prog} to DISA STIG ID this tool handles
+stigid="$(echo "${prog}" | cut -d. -f1)"
+
+
+# Ensure path is robust
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+
+# Define the library include path
+lib_path=${cwd}/../../../libs
+
+# Define the tools include path
+tools_path=${cwd}/../../../stigs
+
+# Define the system backup path
+backup_path=${cwd}/../../../backups/$(uname -n | awk '{print tolower($0)}')
+
+
+# Robot, do work
+
+
+# Error if the ${inc_path} doesn't exist
+if [ ! -d ${lib_path} ] ; then
+  echo "Defined library path doesn't exist (${lib_path})" && exit 1
+fi
+
+
+# Include all .sh files found in ${lib_path}
+incs=($(ls ${lib_path}/*.sh))
+
+# Exit if nothing is found
+if [ ${#incs[@]} -eq 0 ]; then
+  echo "'${#incs[@]}' libraries found in '${lib_path}'" && exit 1
+fi
+
+
+# Iterate ${incs[@]}
+for src in ${incs[@]}; do
+
+  # Make sure ${src} exists
+  if [ ! -f ${src} ]; then
+    echo "Skipping '$(basename ${src})'; not a real file (block device, symlink etc)"
+    continue
+  fi
+
+  # Include $[src} making any defined functions available
+  source ${src}
+
+done
+
+
+# Ensure we have permissions
+if [ $UID -ne 0 ] ; then
+  usage "Requires root privileges" && exit 1
+fi
+
+
+# Set variables
+while getopts "ha:cmvri" OPTION ; do
+  case $OPTION in
+    h) usage && exit 1 ;;
+    a) author=$OPTARG ;;
+    c) change=1 ;;
+    m) meta=1 ;;
+    v) verbose=1 ;;
+    r) restore=1 ;;
+    i) interactive=1 ;;
+    ?) usage && exit 1 ;;
+  esac
+done
+
+
+# Remove once work is complete on module
+cat <<EOF
+[${stigid}] Warning: Not yet implemented...
+
+$(get_meta_data "${cwd}" "${prog}")
+EOF
+exit 1
+
+# Make sure we have an author if we are not restoring or validating
+if [[ "${author}" == "" ]] && [[ ${restore} -ne 1 ]] && [[ ${change} -eq 1 ]]; then
+  usage "Must specify an author name (use -a <initials>)" && exit 1
+fi
+
+# If ${meta} is true
+if [ ${meta} -eq 1 ]; then
+
+  # Print meta data
+  get_meta_data "${cwd}" "${prog}"
+fi
+
+
+# If ${restore} = 1 go to restoration mode
+if [ ${restore} -eq 1 ]; then
+
+  # If ${interactive} = 1 go to interactive restoration mode
+  if [ ${interactive} -eq 1 ]; then
+
+    # Print friendly message regarding restoration mode
+    [ ${verbose} -eq 1 ] && print "Interactive restoration mode for '${file}'"
+
+  fi
+
+  # Print friendly message regarding restoration mode
+  [ ${verbose} -eq 1 ] && print "Restored '${file}'"
+
+  exit 0
+fi
+
+
+# If ${change} = 1
+if [ ${change} -eq 1 ]; then
+
+  # Create backup of file(s), settings or permissions on inodes
+  # (see existing facilities in ${lib_path}/backup.sh)
+
+  # Make change according to ${stigid}
+  [ ${verbose} -eq 1 ] && print "Make change here"
+fi
+
+
+# Validate change according to ${stigid}
+
+
+# Exit 1 if validation failed
+
+
+# Print friendly success
+[ ${verbose} -eq 1 ] && print "Success, conforms to '${stigid}'"
+
+exit 0
+
+# Date: 2017-05-19
+#
+# Severity: CAT-II
+# Classification: UNCLASSIFIED
+# STIG_ID: V00761
+# STIG_Version: SV-38442r2
+# Rule_ID: GEN000300
+#
+# OS: HP-UX
+# Version: 11.31
+# Architecture: 
+#
+# Title: All accounts on the system must have unique user or account names.
+# Description: A unique user name is the first part of the identification and authentication process. If user names are not unique, there can be no accountability on the system for auditing purposes. Multiple accounts sharing the same name could result in the Denial of Service to one or both of the accounts or unauthorized access to files or privileges.
+
