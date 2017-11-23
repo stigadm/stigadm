3d58e2a1ad6d7be5874d585dd101a9062e443c26
Jas <jason.gerfen@gmail.com>
2017-11-23 03:47:25 -0700

    V0047839: Validation & remediation (including backup) completed

--- tools/stigs/Solaris/11/V0047839.sh
+++ tools/stigs/Solaris/11/V0047839.sh
@@ -87,18 +87,18 @@ while getopts "ha:cmvri" OPTION ; do
 done
 
 
-# Remove once work is complete on module
-cat <<EOF
-[${stigid}] Warning: Not yet implemented...
+# Make sure we are operating on global zones
+if [ "$(zonename)" != "global" ]; then
+  print "'${stigid}' only applies to global zones" 1
+  exit 1
+fi
 
-$(get_meta_data "${cwd}" "${prog}")
-EOF
-exit 1
 
 # Make sure we have an author if we are not restoring or validating
 if [[ "${author}" == "" ]] && [[ ${restore} -ne 1 ]] && [[ ${change} -eq 1 ]]; then
   usage "Must specify an author name (use -a <initials>)" && exit 1
 fi
+
 
 # If ${meta} is true
 if [ ${meta} -eq 1 ]; then
@@ -126,21 +126,47 @@ if [ ${restore} -eq 1 ]; then
 fi
 
 
+# Get currently defined audit policies
+policies=( $(auditconfig -getpolicy | grep "^active" | awk '{print $5}' | tr ',' ' ') )
+
+
 # If ${change} = 1
 if [ ${change} -eq 1 ]; then
 
-  # Create backup of file(s), settings or permissions on inodes
-  # (see existing facilities in ${lib_path}/backup.sh)
+  # Create the backup env
+  backup_setup_env "${backup_path}"
 
-  # Make change according to ${stigid}
-  [ ${verbose} -eq 1 ] && print "Make change here"
+  # Create a snapshot of ${users[@]}
+  bu_configuration "${backup_path}" "${author}" "${stigid}" "setpolicy:$(echo "${policies[@]}" | tr ' ' ',')"
+  if [ $? -ne 0 ]; then
+
+    # Print friendly message
+    [ ${verbose} -eq 1 ] && print "Snapshot of current audit flags per zone for '${stigid}' failed..." 1
+
+    # Stop, we require a backup
+    exit 1
+  fi
+
+  # Print friendly message
+  [ ${verbose} -eq 1 ] && print "Created snapshot of current audit flags per zone for '${stigid}'"
+
+
+  # Add the zonename audit flag
+  auditconfig -setpolicy +zonename
+  if [ $? -ne 0 ]; then
+    [ ${verbose} -eq 1 ] && print "Could not add 'zonename' flag to audit policy" 1
+  fi
+
+  # Get currently defined audit policies
+  policies=( $(auditconfig -getpolicy | grep "^active" | awk '{print $5}' | tr ',' ' ') )
 fi
 
 
-# Validate change according to ${stigid}
-
-
-# Exit 1 if validation failed
+# Look for zonename in ${policies[@]} array
+if [ $(in_array "zonename" "${policies[@]}") -eq 1 ]; then
+  [ ${verbose} -eq 1 ] && print "Use of 'zonename' audit flag disabled" 1
+  exit 1
+fi
 
 
 # Print friendly success
@@ -162,4 +188,3 @@ exit 0
 #
 # Title: The audit system must identify in which zone an event occurred.
 # Description: The audit system must identify in which zone an event occurred.
-
