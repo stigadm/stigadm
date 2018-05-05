7ebe8903f964de816d5bd90d7cb1dd3bf9598c7c
jas- <Jason.Gerfen@gmail.com>
2018-05-05 19:34:47 +0400

    V0047857: Fixed quota, compression & reservation calculations

--- tools/stigs/Solaris/11/V0047857.sh
+++ tools/stigs/Solaris/11/V0047857.sh
@@ -1,19 +1,11 @@
-#!/bin/bash -x
+#!/bin/bash
 
-
-# audit min free space
-audit_min_free_space=5
-
-# Quota percentage for auditing ZFS filesystem
-audit_fs_quota=25
 
 # Define an array of ZFS attributes to validate/modify
-#  - Syntax: <ZFS-Attribute>:<Value>
-#  INFO: All sizes should be as percentages
 declare -a zfs_attrs
-zfs_attrs+=('compression:on')
-zfs_attrs+=('reservation:25')
-zfs_attrs+=('quota:25')
+zfs_attrs+=('compression')
+zfs_attrs+=('reservation')
+zfs_attrs+=('quota')
 
 # Define an empty array to hold audit pluign settings
 declare -a audit_settings
@@ -186,35 +178,14 @@ fi
 
 
 # Set ${opts} from ${zfs_attrs[@]} array
-opts="$(echo "${zfs_attrs[@]}" | tr ' ' '\n' | cut -d: -f1 | tr '\n' ',')"
+opts="$(echo "${zfs_attrs[@]}" | tr ' ' ',')"
+
+# Clean up trailing , if any
 opts="$(echo "${opts}" | sed "s|,$||g")"
+
 
 # Obtain an array of ZFS values for the p_file value of ${audit_settings[@]}
 zfs_settings=( $(zfs get ${opts} "${zfs_fs}" | awk '$0 !~ /^NAME/{printf("%s:%s:%s\n", $1, $2, $3)}') )
-
-
-# Get the current total size for ${zfs_fs}
-size="$(zfs list ${zfs_fs} | grep "^${zfs_fs}" | awk '{print $3}')"
-
-# Get the current free size for ${zfs_fs}
-free_size="$(zfs list ${zfs_fs} | grep "^${zfs_fs}" | awk '{print $4}')"
-
-
-# Get the size type from ${total}
-size_type="$(echo "${size}" | sed "s|.*\([A-Z]\)$|\1|g")"
-
-# Remove ${size_type} from ${size} to get ${total}
-total="$(echo "${size}" | sed "s|${size_type}||g")"
-
-# Convert ${total} to bytes
-total_bytes=$(tobytes "${size_type}" ${total})
-
-# Get the percentage of bytes based on ${audit_fs_quota} & ${total_bytes}
-quota_size="$(frombytes "${size_type}" $(percent ${total_bytes} ${audit_fs_quota}))${size_type}"
-
-
-# Get the parent folder for ${zfs_fs} & it's available size
-zfs_fs_parent="$(zfs list $(dirname ${zfs_fs}) | awk '$0 !~ /^NAME/{printf("%s:%s:%s\n", $1, $3, $5)}')"
 
 
 # If ${change} = 1
@@ -273,60 +244,8 @@ if [ ${change} -eq 1 ]; then
   fi
 
 
-  # Iterate ${zfs_attrs[@]}
-  for attr in ${zfs_attrs[@]}; do
-
-    # Split up ${attr} into the key & desired value
-    key="$(echo "${attr}" | cut -d: -f1)"
-    value="$(echo "${attr}" | cut -d: -f2)"
-
-    # Test ${value} for an integer & apply a percentage
-    if [ $(is_int ${value}) -eq 1 ]; then
-
-      # Copy ${value}
-      tvalue=${value}
-
-      # Convert ${total_bytes} to a percentage of ${value}
-      value="$(frombytes "${size_type}" $(percent ${total_bytes} ${value}))"
-
-      # If ${value} is 0 then skip & notify of issue
-      if [ ${value} -eq 0 ]; then
-
-        [ ${verbose} -eq 1 ] && print "Calculations for '${key} [${total_bytes} / 100 * ${tvalue}]' resulted in '${value}', skipping" 1
-        continue
-      fi
-
-      # Apply ${size_type} to ${value}
-      value="${value}${size_type}"
-    fi
-
-    # Enable compression on the ZFS audit fs
-    zfs set ${key}=${value} ${zfs_fs} 2>/dev/null
-    if [ $? -ne 0 ]; then
-
-      # Print friendly message
-      [ ${verbose} -eq 1 ] && print "Could set '${key}' to '${value}' on '${zfs_fs}'..." 1
-
-      continue
-    fi
-
-    # Print friendly message
-    [ ${verbose} -eq 1 ] && print "Set '${key}' to '${value}' on '${zfs_fs}'..."
-  done
-
-
   # Obtain an array of audit settings regarding 'bin_file' plugin
   audit_settings=( $(auditconfig -getplugin audit_binfile | awk '$0 ~ /Attributes/{print $2}' | tr ';' ' ' | tr '=' ':') )
-
-  # Get the auditing min_free value from ${audit_settings[@]}
-  fs_free="$(echo "${audit_settings[@]}" | tr ' '  '\n' | grep "^p_minfree" | cut -d: -f2)"
-
-  # Create a copy of ${zfs_settings[@]} prior to over writting it
-  declare -a orig_zfs_settings
-  orig_zfs_settings=( ${zfs_settings} )
-
-  # Obtain an array of ZFS values for the p_file value of ${audit_settings[@]}
-  zfs_settings=( $(zfs get ${opts} "${zfs_fs}" | awk '$0 !~ /^NAME/{printf("%s:%s:%s\n", $1, $2, $3)}') )
 fi
 
 
@@ -334,67 +253,15 @@ fi
 declare -a errors
 
 
-# Get the current p_minfree value
-cur_audit_min_free_space=$(echo "${audit_settings[@]}" | tr ' ' '\n' | grep "p_minfree" | cut -d: -f2)
-
-# Validate p_minfree value for audit_binfile
-if [ ${cur_audit_min_free_space} -ne ${audit_min_free_space} ]; then
-  errors+=("p_minfree:${audit_min_free_space}:${cur_audit_min_free_space}")
-fi
-
-
 # Iterate ${zfs_attrs[@]}
 for attr in ${zfs_attrs[@]}; do
 
-  # Split up ${attr} into the key & desired value
-  key="$(echo "${attr}" | cut -d: -f1)"
-  value="$(echo "${attr}" | cut -d: -f2)"
-
   # Get the current ${key} from ${zfs_settings[@]}
-  cur_value="$(echo "${zfs_settings[@]}" | tr ' ' '\n' | grep "${key}" | cut -d: -f3)"
+  cur_value="$(echo "${zfs_settings[@]}" | tr ' ' '\n' | grep "${attr}" | cut -d: -f3)"
 
-
-  # Test ${value} for an integer & apply a percentage
-  if [ $(is_int ${value}) -eq 1 ]; then
-
-    # Copy ${value}
-    tvalue=${value}
-
-    # If ${value} starts with a number
-    if [ $(echo "${value}" | awk '{if ($0 ~ /^[0-9]/){print 1}else{print 0}}') -eq 1 ]; then
-
-      # Get the current size for the parent file system
-      parent_size="$(echo "${zfs_fs_parent}" | cut -d: -f2)"
-
-      # Get the size type from ${total}
-      parent_size_type="$(echo "${parent_size}" | sed "s|.*\([A-Z]\)$|\1|g")"
-
-      # Remove ${size_type} from ${size} to get ${total}
-      parent_total="$(echo "${parent_size}" | sed "s|${parent_size_type}||g")"
-
-      # Convert ${total} to bytes
-      parent_total_bytes=$(tobytes "${parent_size_type}" ${parent_total})
-
-      # Add ${parent_total_bytes} with ${cur_total_bytes}
-      parent_total_bytes=$(add ${parent_total_bytes} ${total_bytes})
-
-      # Get the percentage of bytes based on ${audit_fs_quota} & ${total_bytes}
-      parent_quota_size="$(frombytes "${parent_size_type}" $(percent ${parent_total_bytes} ${audit_fs_quota}))${parent_size_type}"
-
-
-      # If ${parent_quota_size} != ${cur_value}
-      if [ $(echo "${cur_value}" | grep -c "^${parent_quota_size}$") -eq 0 ]; then
-#      if [ "${parent_quota_size}${parent_size_type}" != "${cur_value}" ]; then
-
-        errors+=("${zfs_fs}:${key}:${value}")
-        continue
-      fi
-    fi
-
-    # Test ${cur_value} against ${value}
-    if [ "${cur_value}" != "${value}" ]; then
-      errors+=("${zfs_fs}:${key}:${cur_value}")
-    fi
+  # Test ${cur_value}
+  if [ $(echo "${cur_value}" | egrep -c 'off|none') -gt 0 ]; then
+    errors+=("${zfs_fs}:${attr}:${cur_value}")
   fi
 done
 
