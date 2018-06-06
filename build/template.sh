08387aa7ea779a591187e419b07ce20ca3fd2a26
jas- <Jason.Gerfen@gmail.com>
2018-06-06 19:10:23 +0400

    v0.1.2: Removed OS STIGS not yet implemented

--- build/template.sh
+++ build/template.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+###############################################
+# Bootstrapping environment setup
+###############################################
+
 # Get our working directory
 cwd="$(pwd)"
 
@@ -15,11 +20,19 @@ fi
 source ${bootstrap}
 
 
+###############################################
+# Global zones only check
+###############################################
+
 # Make sure we are operating on global zones
 if [ "$(zonename)" != "global" ]; then
   usage "${stigid} only applies to global zones" && exit 1
 fi
 
+
+###############################################
+# Metrics start
+###############################################
 
 # Get EPOCH
 s_epoch="$(gen_epoch)"
@@ -27,64 +40,116 @@ s_epoch="$(gen_epoch)"
 # Create a timestamp
 timestamp="$(gen_date)"
 
-
-# Remove once work is complete on module
-cat <<EOF
-[${stigid}] Warning: Not yet implemented...
-
-$(get_meta_data "${cwd}" "${prog}")
-EOF
-exit 1
-
-# Make sure we have an author if we are not restoring or validating
-if [[ "${author}" == "" ]] && [[ ${restore} -ne 1 ]] && [[ ${change} -eq 1 ]]; then
-  usage "Must specify an author name (use -a <initials>)" && exit 1
-fi
-
-# If ${meta} is true
-if [ ${meta} -eq 1 ]; then
-
-  # Print meta data
-  get_meta_data "${cwd}" "${prog}"
-fi
+# Whos is calling? 0 = singular, 1 is as group
+caller=$(ps $PPID | grep -c stigadm)
 
 
-# If ${restore} = 1 go to restoration mode
+###############################################
+# STIG validation/remediation/restoration
+###############################################
+
+# Define a validated status
+status=0
+
+# Perform restoration if requested
 if [ ${restore} -eq 1 ]; then
 
-  # If ${interactive} = 1 go to interactive restoration mode
-  if [ ${interactive} -eq 1 ]; then
-
-    # Print friendly message regarding restoration mode
-    [ ${verbose} -eq 1 ] && print "Interactive restoration mode for '${file}'"
-
-  fi
-
-  # Print friendly message regarding restoration mode
-  [ ${verbose} -eq 1 ] && print "Restored '${file}'"
-
-  exit 0
+  exit 0 # 0 for success, 1 for error
 fi
 
 
-# If ${change} = 1
+# Perform initial STIG validation here
+
+
+# If ${change} == 1
 if [ ${change} -eq 1 ]; then
 
-  # Create backup of file(s), settings or permissions on inodes
-  # (see existing facilities in ${lib_path}/backup.sh)
-
-  # Make change according to ${stigid}
-  [ ${verbose} -eq 1 ] && print "Make change here"
+  # Perform STIG remediation here
+  echo "NOT YET IMPLEMENTED"
 fi
 
 
-# Validate change according to ${stigid}
+###############################################
+# Finish metrics
+###############################################
+
+# Get EPOCH
+e_epoch="$(gen_epoch)"
+
+# Determine miliseconds from start
+seconds=$(subtract ${s_epoch} ${e_epoch})
+
+# Generate a run time
+[ ${seconds} -gt 60 ] && run_time="$(divide ${seconds} 60) Min." || run_time="${seconds} Sec."
 
 
-# Exit 1 if validation failed
+###############################################
+# Results for printable report
+###############################################
+
+# If ${status} != 1
+if [ ${status:=0} -ne 1 ]; then
+
+  # Set ${results} error message
+  results="Failed validation"
+
+  # Populate a value in ${errors[@]} if ${caller} is > 0
+  [ ${caller} -gt 0 ] && errors=("${stigid}")
+fi
+
+# Set ${results} passed message
+[ ${status} -eq 1 ] && results="Passed validation"
 
 
-# Print friendly success
-[ ${verbose} -eq 1 ] && print "Success, conforms to '${stigid}'"
+###############################################
+# Report generation specifics
+###############################################
 
-exit 0
+# If ${caller} = 0
+if [ ${caller} -eq 0 ]; then
+
+  # Apply some values expected for general report
+  stigs=("${stigid}")
+  total_stigs=${#stigs[@]}
+
+  # Generate the primary report header
+  report_header
+fi
+
+# Capture module report to ${log}
+module_header "${results}"
+
+# Provide detailed results to ${log}
+if [ ${verbose} -eq 1 ]; then
+
+  # Print a singular line based on ${log} extention
+  print_line ${log} "details" "${blob}"
+fi
+
+# Print the modul footer
+module_footer
+
+if [ ${caller} -eq 0 ]; then
+
+  # Apply some values expected for report footer
+  [ ${status} -eq 1 ] && passed=${status} || passed=0
+  [ ${status} -eq 1 ] && failed=0 || failed=${status}
+
+  # Calculate a percentage from applied modules & errors incurred
+  percentage=$(percent ${passed} ${failed})
+
+  # Print the report footer
+  report_footer
+
+  # Print ${log} since we were called alone
+  cat ${log}
+fi
+
+
+###############################################
+# Return code for larger report
+###############################################
+
+# Return an error/success code (0/1)
+[ ${status} -eq 1 ] && exit 0 || exit 1
+
