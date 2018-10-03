0968cebdcee950a302e3302d68eca7d0a150f185
jas- <Jason.Gerfen@gmail.com>
2018-10-03 04:41:07 -0600

    v0.1.3: V0047863: Migrated to new API; tested validaiton/remediation

--- tools/stigs/Solaris/11/V0047863.sh
+++ tools/stigs/Solaris/11/V0047863.sh
@@ -5,154 +5,69 @@ declare -a policy
 policy+=("ahlt")
 
 
-# Global defaults for tool
-author=
-change=0
-json=1
-meta=0
-restore=0
-interactive=0
-xml=0
+###############################################
+# Bootstrapping environment setup
+###############################################
 
+# Get our working directory
+cwd="$(pwd)"
 
-# Working directory
-cwd="$(dirname $0)"
+# Define our bootstrapper location
+bootstrap="${cwd}/tools/bootstrap.sh"
 
-# Tool name
-prog="$(basename $0)"
-
-
-# Copy ${prog} to DISA STIG ID this tool handles
-stigid="$(echo "${prog}" | cut -d. -f1)"
-
-
-# Ensure path is robust
-PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-
-
-# Define the library include path
-lib_path=${cwd}/../../../libs
-
-# Define the tools include path
-tools_path=${cwd}/../../../stigs
-
-# Define the system backup path
-backup_path=${cwd}/../../../backups/$(uname -n | awk '{print tolower($0)}')
-
-
-# Robot, do work
-
-
-# Error if the ${inc_path} doesn't exist
-if [ ! -d ${lib_path} ] ; then
-  echo "Defined library path doesn't exist (${lib_path})" && exit 1
+# Bail if it cannot be found
+if [ ! -f ${bootstrap} ]; then
+  echo "Unable to locate bootstrap; ${bootstrap}" && exit 1
 fi
 
-
-# Include all .sh files found in ${lib_path}
-incs=($(ls ${lib_path}/*.sh))
-
-# Exit if nothing is found
-if [ ${#incs[@]} -eq 0 ]; then
-  echo "'${#incs[@]}' libraries found in '${lib_path}'" && exit 1
-fi
+# Load our bootstrap
+source ${bootstrap}
 
 
-# Iterate ${incs[@]}
-for src in ${incs[@]}; do
-
-  # Make sure ${src} exists
-  if [ ! -f ${src} ]; then
-    echo "Skipping '$(basename ${src})'; not a real file (block device, symlink etc)"
-    continue
-  fi
-
-  # Include $[src} making any defined functions available
-  source ${src}
-
-done
-
-
-# Ensure we have permissions
-if [ $UID -ne 0 ] ; then
-  usage "Requires root privileges" && exit 1
-fi
-
-
-# Set variables
-while getopts "ha:cjmvrix" OPTION ; do
-  case $OPTION in
-    h) usage && exit 1 ;;
-    a) author=$OPTARG ;;
-    c) change=1 ;;
-    j) json=1 ;;
-    m) meta=1 ;;
-    r) restore=1 ;;
-    i) interactive=1 ;;
-    x) xml=1 ;;
-    ?) usage && exit 1 ;;
-  esac
-done
-
-
-# Make sure we have an author if we are not restoring or validating
-if [[ "${author}" == "" ]] && [[ ${restore} -ne 1 ]] && [[ ${change} -eq 1 ]]; then
-  usage "Must specify an author name (use -a <initials>)" && exit 1
-fi
-
-
-# Make sure we have required defined values
-if [ ${#policy[@]} -eq 0 ]; then
-
-  print "Requires one or more default policies defined" 1
-  exit 1
-fi
-
-
-# If ${meta} is true
-if [ ${meta} -eq 1 ]; then
-
-  # Print meta data
-  get_meta_data "${cwd}" "${prog}"
-fi
-
+###############################################
+# Global zones only check
+###############################################
 
 # Make sure we are operating on global zones
 if [ "$(zonename)" != "global" ]; then
-  print "'${stigid}' only applies to global zones" 1
-  exit 1
+  report "${stigid} only applies to global zones" && exit 1
 fi
 
+
+###############################################
+# Metrics start
+###############################################
+
+# Get EPOCH
+s_epoch="$(gen_epoch)"
+
+# Create a timestamp
+timestamp="$(gen_date)"
+
+# Whos is calling? 0 = singular, 1 is as group
+caller=$(ps $PPID | grep -c stigadm)
+
+
+###############################################
+# Perform restoration
+###############################################
 
 # If ${restore} = 1 go to restoration mode
-if [[ ${restore} -eq 1 ]] && [[ ${cond} -eq 1 ]]; then
-
-  # If ${interactive} = 1 go to interactive restoration mode
-  if [ ${interactive} -eq 1 ]; then
-
-    # Print friendly message regarding restoration mode
-    [ ${verbose} -eq 1 ] && print "Interactive restoration mode for '${file}'"
-
-  fi
-
-  # Do work
-  audit -t
-  if [ $? -ne 0 ]; then
-
-    # Print friendly message
-    [ ${verbose} -eq 1 ] && print "Unable to disable auditing" 1
-    exit 1
-  fi
-
-  exit 0
+if [ ${restore} -eq 1 ]; then
+  report "Not yet implemented" && exit 1
 fi
 
+
+###############################################
+# STIG validation/remediation
+###############################################
 
 # If ${change} == 1
 if [ ${change} -eq 1 ]; then
 
   # Get an array of default policy flags
-  cur_policy=($(auditconfig -getpolicy | awk '$1 ~ /^active/{print}' | cut -d= -f2 | tr ',' ' '))
+  cur_policy=($(auditconfig -getpolicy |
+    awk '$1 ~ /^active/{print}' | cut -d= -f2 | tr ',' ' '))
 
 
   # Create the backup env
@@ -167,14 +82,11 @@ if [ ${change} -eq 1 ]; then
   if [ $? -ne 0 ]; then
 
     # Print friendly message
-    [ ${verbose} -eq 1 ] && print "Snapshot of current audit flags for '${stigid}' failed..."
+    report "Snapshot of current audit flags for '${stigid}' failed..."
 
     # Stop, we require a backup
     exit 1
   fi
-
-  # Print friendly message
-  [ ${verbose} -eq 1 ] && print "Created snapshot of current default audit flags & policies"
 
 
   # Combine & remove duplicates from ${policy[@]} & ${cur_policy[@]}
@@ -187,17 +99,9 @@ if [ ${change} -eq 1 ]; then
   # Set the value(s) to the audit service
   auditconfig -setpolicy ${defpol} &>/dev/null
 
-  # Handle results
-  if [ $? -ne 0 ]; then
-
-    # Print friendly message
-    [ ${verbose} -eq 1 ] && print "An error occurred setting default audit policy: ${defpol}" 1
-  fi
+  # Trap error
+  [ $? -ne 0 ] && errors+=("Failed:set:auditconfig:setpolicy:${defpol}")
 fi
-
-
-# Declare an empty array for errors
-declare -a err
 
 
 # Get an array of default policy flags
@@ -207,37 +111,81 @@ cur_policy=($(auditconfig -getpolicy 2>/dev/null | awk '$1 ~ /^active/{print}' |
 for pol in ${policy[@]}; do
 
   # Check for ${flag} in ${cur_policy[@]}
-  [ $(in_array "${pol}" "${cur_policy[@]}") -eq 1 ] && err+=("policy:${pol}")
+  [ $(in_array "${pol}" "${cur_policy[@]}") -eq 1 ] && errors+=("policy:${pol}")
+
+  # Show inspected
+  inspected+=("auditconfig:getpolicy:${pol}")
 done
 
 
-# If ${#err[@]} > 0
-if [ ${#err[@]} -gt 0 ]; then
+###############################################
+# Results for printable report
+###############################################
 
-  # Print friendly message
-  print "Current audit settings does not conform to '${stigid}'" 1
+# If ${#errors[@]} > 0
+if [ ${#errors[@]} -gt 0 ]; then
 
-  # Iterate ${err[@]}
-  for error in ${err[@]}; do
+  # Set ${results} error message
+  results="Failed validation"
+fi
 
-    # Get setting from ${error}
-    setting="$(echo "${error}" | cut -d: -f1)"
+# Set ${results} passed message
+[ ${#errors[@]} -eq 0 ] && results="Passed validation"
 
-    # Get options from ${error}
-    option="$(echo "${error}" | cut -d: -f2)"
 
-    # Print friendly message
-    print "  ${setting} (${option})" 1
-  done | sort -u
+###############################################
+# Report generation specifics
+###############################################
 
-  exit 1
+# Apply some values expected for report footer
+[ ${#errors[@]} -eq 0 ] && passed=1 || passed=0
+[ ${#errors[@]} -gt 0 ] && failed=${#errors[@]} || failed=0
+
+# Calculate a percentage from applied modules & errors incurred
+percentage=$(percent ${passed} ${failed})
+
+
+# If the caller was only independant
+if [ ${caller} -eq 0 ]; then
+
+  # Provide detailed results to ${log}
+  if [ ${verbose} -eq 1 ]; then
+
+    # Print array of failed & validated items
+    [ ${#errors[@]} -gt 0 ] && print_array ${log} "errors" "${errors[@]}"
+    [ ${#inspected[@]} -gt 0 ] && print_array ${log} "validated" "${inspected[@]}"
+  fi
+
+  # Generate the report
+  report "${results}"
+
+  # Display the report
+  cat ${log}
+else
+
+  # Since we were called from stigadm
+  module_header "${results}"
+
+  # Provide detailed results to ${log}
+  if [ ${verbose} -eq 1 ]; then
+
+    # Print array of failed & validated items
+    [ ${#errors[@]} -gt 0 ] && print_array ${log} "errors" "${errors[@]}"
+    [ ${#inspected[@]} -gt 0 ] && print_array ${log} "validated" "${inspected[@]}"
+  fi
+
+  # Finish up the module specific report
+  module_footer
 fi
 
 
-# Print friendly success
-[ ${verbose} -eq 1 ] && print "Success, conforms to '${stigid}'"
+###############################################
+# Return code for larger report
+###############################################
 
-exit 0
+# Return an error/success code (0/1)
+exit ${#errors[@]}
+
 
 # Date: 2017-06-21
 #
