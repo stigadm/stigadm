66d1ee10c6b9c50fae56f1ccb73d1a7d9869b989
jas- <Jason.Gerfen@gmail.com>
2018-10-06 10:26:52 -0600

    v0.1.3: V0047899: In progress: - Defined the following resource restrictions:   - UID/GID range for projects group resource management   - Whitelist of applicable users/groups   - Application only account resource limitations   - End user only account resource limitations   - Per zone user/group (both application/end users) resource limitations   - Per network interface (whitelist) security protections & bandwidth limits

--- tools/stigs/Solaris/11/V0047899.sh
+++ tools/stigs/Solaris/11/V0047899.sh
@@ -1,75 +1,83 @@
 #!/bin/bash
 
+# Define an whitelist of accounts to ignore
+declare -a whitelist
+whitelist+=("root")
+
+# Define a range of UID/GID's to associate with the ${application_accounts[@]} array
+declare -A appliction_acct_range
+application_acct_range['min']=100
+application_acct_range['max']=500
+
+# Define a range of UID's to associat with the ${user_accounts[@]} array
+declare -A user_acct_range
+user_acct_range['min']=501
+user_acct_range['max']=2147483647
+
 
 # Boolean true/false to set properties per configured zone
-config_per_zone="true"
+config_per_zone=true
 
+# Define a whitelist of VNIC's to exclude from ${network_properties[@]}
+#  NOTE: This also filters existing interfaces from zones
+declare -a network_whitelist
+network_whitelist+=("net0")
+network_whitelist+=("vnic0")
+
+
+# An associative array of project attributes for application user accounts
+declare -A application_accounts
+application_accounts["process.max-file-descriptor"]=64000
+application_accounts["process.max-stack-size"]="80%"
+application_accounts["process.max-address-space"]="90%"
+application_accounts["project.max-locked-memory"]="80%"
+application_accounts["project.cpu-shares"]="80%"
+application_accounts["project.max-shm-ids"]=64000
+application_accounts["project.max-shm-memory"]="80%"
+application_accounts["project.max-tasks"]=64000
+application_accounts["project.max-lwps"]=64000
+application_accounts["task.max-processes"]=64000
+
+
+# An associative array of project attributes for end user accounts
+declare -A user_accounts
+user_accounts["process.max-file-descriptor"]=32000
+user_accounts["process.max-stack-size"]="75%"
+user_accounts["process.max-address-space"]="75%"
+user_accounts["project.cpu-shares"]="75%"
+user_accounts["project.max-locked-memory"]="75%"
+user_accounts["project.max-shm-ids"]=32000
+user_accounts["project.max-shm-memory"]="75%"
+user_accounts["project.max-tasks"]=32000
+user_accounts["project.max-lwps"]=32000
+user_accounts["task.max-processes"]=32000
+
+
+# An associative array of zone specific project limits
+declare -A zone_limits
+zone_limits["zone.cpu-shares"]="80%"
+zone_limits["zone.max-lofi"]=8
+zone_limits["zone.max-lwps"]=64000
+zone_limits["zone.max-processes"]=64000
+zone_limits["zone.max-shm-ids"]=64000
+zone_limits["zone.max-shm-memory"]="80%"
+zone_limits["zone.max-swap"]="80%"
+
+
+# Define an associative array of virtual nic properties
+declare -A network_properties
+network_properties["zone"]=true # Limit VNIC to zone where zone is configured to use vnic
+network_properties["protection"]="mac-nospoof,restricted,ip-nospoof,dhcp-nospoof" # See `man dladm` for info
+network_properties["allowed-ips"]=true # Limits allowed outbound SRC datagrams on list ONLY (Acquires from configured IP's)
+network_properties["maxbw"]="75%" # Use ${network_whitelist[@]} array to exclude VNIC's, otherwise limit bandwidth
 
 # Set some default actions
 priv_level="priv"
 log_level="warning"
 action="deny"
 
-
 # Define a log level for rctladm
 log_level="syslog"
-
-
-# An associative array of project attributes for application user accounts
-declare -A application_accounts
-application_accounts["process.max-file-descriptor"]=16000
-application_accounts["process.max-stack-size"]="70%"
-application_accounts["process.max-address-space"]="60%"
-application_accounts["project.max-locked-memory"]="65%"
-application_accounts["project.cpu-shares"]="80%"
-application_accounts["project.max-shm-ids"]=64000
-application_accounts["project.max-shm-memory"]="75%"
-application_accounts["project.max-tasks"]=32000
-application_accounts["project.max-lwps"]=32000
-application_accounts["task.max-processes"]=16000
-
-
-# An associative array of project attributes for end user accounts
-declare -A user_accounts
-user_accounts["process.max-file-descriptor"]=16000
-user_accounts["process.max-stack-size"]="50%"
-user_accounts["process.max-address-space"]="50%"
-user_accounts["project.cpu-shares"]="60%"
-user_accounts["project.max-locked-memory"]="40%"
-user_accounts["project.max-shm-ids"]=16000
-user_accounts["project.max-shm-memory"]="60%"
-user_accounts["project.max-tasks"]=16000
-user_accounts["project.max-lwps"]=16000
-user_accounts["task.max-processes"]=16000
-
-
-# An associative array of zone specific project limits
-declare -A zone_limits
-zone_limits["zone.cpu-shares"]="75%"
-zone_limits["zone.max-lofi"]=8
-zone_limits["zone.max-lwps"]=32000
-zone_limits["zone.max-processes"]=64000
-zone_limits["zone.max-shm-ids"]=64000
-zone_limits["zone.max-shm-memory"]="75%"
-zone_limits["zone.max-swap"]="70%"
-
-
-# Define an associative array of virtual nic properties
-declare -A network_properties
-network_properties["zone"]="true" # Limit VNIC to zone where zone is configured to use vnic
-network_properties["protection"]="mac-nospoof,restricted,ip-nospoof,dhcp-nospoof" # See `man dladm` for info
-network_properties["allowed-ips"]="true" # Limits allowed outbound SRC datagrams on list ONLY (Acquires from configured IP's)
-network_properties["maxbw"]="75%" # Use ${network_whitelist[@]} array to exclude VNIC's, otherwise limit bandwidth
-
-
-# Define an whitelist of accounts to ignore
-declare -a whitelist
-whitelist+=("root")
-
-
-# Define a whitelist of VNIC's to exclude from ${network_properties[@]}
-declare -a network_whitelist
-network_whitelist+=("vnic1")
 
 
 # Define the project file
@@ -80,130 +88,106 @@ file=/etc/project
 nsswitch=/etc/nsswitch.conf
 
 
-# Global defaults for tool
-author=
-change=0
-json=1
-meta=0
-restore=0
-interactive=0
-xml=0
+###############################################
+# Bootstrapping environment setup
+###############################################
+
+# Get our working directory
+cwd="$(pwd)"
+
+# Define our bootstrapper location
+bootstrap="${cwd}/tools/bootstrap.sh"
+
+# Bail if it cannot be found
+if [ ! -f ${bootstrap} ]; then
+  echo "Unable to locate bootstrap; ${bootstrap}" && exit 1
+fi
+
+# Load our bootstrap
+source ${bootstrap}
 
 
-# Working directory
-cwd="$(dirname $0)"
+###############################################
+# Global zones only check
+###############################################
 
-# Tool name
-prog="$(basename $0)"
-
-
-# Copy ${prog} to DISA STIG ID this tool handles
-stigid="$(echo "${prog}" | cut -d. -f1)"
-
-
-# Ensure path is robust
-PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-
-
-# Define the library include path
-lib_path=${cwd}/../../../libs
-
-# Define the tools include path
-tools_path=${cwd}/../../../stigs
-
-# Define the system backup path
-backup_path=${cwd}/../../../backups/$(uname -n | awk '{print tolower($0)}')
-
-
-# Robot, do work
-
-
-# Error if the ${inc_path} doesn't exist
-if [ ! -d ${lib_path} ] ; then
-  echo "Defined library path doesn't exist (${lib_path})" && exit 1
+# Make sure we are operating on global zones
+if [ "$(zonename)" != "global" ]; then
+  report "${stigid} only applies to global zones" && exit 1
 fi
 
 
-# Include all .sh files found in ${lib_path}
-incs=($(ls ${lib_path}/*.sh))
+###############################################
+# Metrics start
+###############################################
 
-# Exit if nothing is found
-if [ ${#incs[@]} -eq 0 ]; then
-  echo "'${#incs[@]}' libraries found in '${lib_path}'" && exit 1
-fi
+# Get EPOCH
+s_epoch="$(gen_epoch)"
 
+# Create a timestamp
+timestamp="$(gen_date)"
 
-# Iterate ${incs[@]}
-for src in ${incs[@]}; do
-
-  # Make sure ${src} exists
-  if [ ! -f ${src} ]; then
-    echo "Skipping '$(basename ${src})'; not a real file (block device, symlink etc)"
-    continue
-  fi
-
-  # Include $[src} making any defined functions available
-  source ${src}
-
-done
+# Whos is calling? 0 = singular, 1 is as group
+caller=$(ps $PPID | grep -c stigadm)
 
 
-# Ensure we have permissions
-if [ $UID -ne 0 ] ; then
-  usage "Requires root privileges" && exit 1
-fi
-
-
-# Set variables
-while getopts "ha:cjmvrix" OPTION ; do
-  case $OPTION in
-    h) usage && exit 1 ;;
-    a) author=$OPTARG ;;
-    c) change=1 ;;
-    j) json=1 ;;
-    m) meta=1 ;;
-    r) restore=1 ;;
-    i) interactive=1 ;;
-    x) xml=1 ;;
-    ?) usage && exit 1 ;;
-  esac
-done
-
-
-# Make sure we have an author if we are not restoring or validating
-if [[ "${author}" == "" ]] && [[ ${restore} -ne 1 ]] && [[ ${change} -eq 1 ]]; then
-  usage "Must specify an author name (use -a <initials>)" && exit 1
-fi
-
-# If ${meta} is true
-if [ ${meta} -eq 1 ]; then
-
-  # Print meta data
-  get_meta_data "${cwd}" "${prog}"
-fi
-
+###############################################
+# Perform restoration
+###############################################
 
 # If ${restore} = 1 go to restoration mode
 if [ ${restore} -eq 1 ]; then
-
-  # If ${interactive} = 1 go to interactive restoration mode
-  if [ ${interactive} -eq 1 ]; then
-
-    # Print friendly message regarding restoration mode
-    [ ${verbose} -eq 1 ] && print "Interactive restoration mode for '${file}'"
-
-  fi
-
-  # Print friendly message regarding restoration mode
-  [ ${verbose} -eq 1 ] && print "Restored '${file}'"
-
-  exit 0
+  report "Not yet implemented" && exit 1
 fi
 
 
+###############################################
+# STIG validation/remediation
+###############################################
+
 # Get total number of CPU's
+cpus=$(psrinfo | wc -l)
+
 # Get total amount of physical memory
-# Get number of configured zones
+memory=$(prtconf | awk '$1 ~ /Memory/{printf("%s\n", $3)}')
+
+# Get an array of configured zones (excluding root)
+zones=( $(zoneadm list -civ |
+  awk '$0 !~ /global/{printf("%s:%s\n", $2, $4)}') )
+
+
+# Iterate ${network_whitelist[@]} array
+for item in ${network_whitelist[@]}; do
+
+  # Iterate ${network_properties[@]}
+  for property in ${!network_properties[@]}; do
+
+    # Get the current allowed-ips values
+    current_allowed_ips+=( $(dladm show-linkprop -p ${property} -o link,property,effective ${item} 2>/dev/null |
+      nawk 'NR > 1{nic=$1;for(i = NR; i <= NR; i++){if($1 ~ /^[[0-9]+\./){ val=val$1 }else{ val=nic":"$3 } print val}}' | tail -1) )
+
+    # Trap errors for missing ${item}
+    [ $? -ne 0 ] && errors+=("Missing:interface:${item}")
+
+
+    # Iterate ${zones[@]}
+    for zone in ${zones[@]}; do
+
+      # Get the current allowed-ips values for ${zone}
+      current_allowed_ips+=( $(dladm show-linkprop -p ${property} -o link,property,effective -z ${zone} ${item} 2>/dev/null |
+        nawk 'NR > 1{nic=$1;for(i = NR; i <= NR; i++){if($1 ~ /^[[0-9]+\./){ val=val$1 }else{ val=nic":"$3 } print val}}' | tail -1) )
+
+      # Trap errors for missing ${item}
+      [ $? -ne 0 ] && errors+=("Missing:interface:${item}:in:${zone}")
+    done
+  done
+done
+
+# Remove dupes
+current_allowed_ips=( $(remove_duplicates  "${current_allowed_ips[@]}") )
+
+
+
 
 # Calculate percentages for the following:
 #  - CPU / [Memory | Zone] / X (Where X is the project|network limit)
@@ -219,47 +203,82 @@ if [ ${change} -eq 1 ]; then
   #bu_configuration "${backup_path}" "${author}" "${stigid}" "$(echo "${pkgs[@]}" | tr ' ' '\n')"
   if [ $? -ne 0 ]; then
 
-    # Print friendly message
-    [ ${verbose} -eq 1 ] && print "Snapshot of broken packages failed..." 1
+    # Verbose
+    report "Snapshot of broken packages failed..." 1
 
     # Stop, we require a backup
     exit 1
   fi
-
-  # Print friendly message
-  [ ${verbose} -eq 1 ] && print "Created snapshot of broken packages"
 fi
 
+
+###############################################
+# Results for printable report
+###############################################
 
 # If ${#errors[@]} > 0
 if [ ${#errors[@]} -gt 0 ]; then
 
-  # Print friendly success
-  [ ${verbose} -eq 1 ] && print "Does not conform to '${stigid}'" 1
+  # Set ${results} error message
+  results="Failed validation"
+fi
 
-  # Iterate ${errors[@]}
-  for error in ${errors[@]}; do
+# Set ${results} passed message
+[ ${#errors[@]} -eq 0 ] && results="Passed validation"
 
-    key="$(echo "${error}" | cut -d: -f1,2)"
-    inode="$(echo "${error}" | cut -d: -f3)"
-    flag="$(echo "${error}" |  cut -d: -f4)"
-    cvalue="$(echo "${error}" | cut -d: -f5)"
-    value="$(echo "${error}" | cut -d: -f6)"
 
-    # Print friendly success
-    [ ${verbose} -eq 1 ] && print "  Package: ${key}" 1
-    [ ${verbose} -eq 1 ] && print "    Inode: ${inode}" 1
-    [ ${verbose} -eq 1 ] && print "      Type: ${type} ${cvalue} [${value}]" 1
-  done
+###############################################
+# Report generation specifics
+###############################################
 
-  exit 1
+# Apply some values expected for report footer
+[ ${#errors[@]} -eq 0 ] && passed=1 || passed=0
+[ ${#errors[@]} -gt 0 ] && failed=1 || failed=0
+
+# Calculate a percentage from applied modules & errors incurred
+percentage=$(percent ${passed} ${failed})
+
+
+# If the caller was only independant
+if [ ${caller} -eq 0 ]; then
+
+  # Provide detailed results to ${log}
+  if [ ${verbose} -eq 1 ]; then
+
+    # Print array of failed & validated items
+    [ ${#errors[@]} -gt 0 ] && print_array ${log} "errors" "${errors[@]}"
+    [ ${#inspected[@]} -gt 0 ] && print_array ${log} "validated" "${inspected[@]}"
+  fi
+
+  # Generate the report
+  report "${results}"
+
+  # Display the report
+  cat ${log}
+else
+
+  # Since we were called from stigadm
+  module_header "${results}"
+
+  # Provide detailed results to ${log}
+  if [ ${verbose} -eq 1 ]; then
+
+    # Print array of failed & validated items
+    [ ${#errors[@]} -gt 0 ] && print_array ${log} "errors" "${errors[@]}"
+    [ ${#inspected[@]} -gt 0 ] && print_array ${log} "validated" "${inspected[@]}"
+  fi
+
+  # Finish up the module specific report
+  module_footer
 fi
 
 
-# Print friendly success
-[ ${verbose} -eq 1 ] && print "Success, conforms to '${stigid}'"
+###############################################
+# Return code for larger report
+###############################################
 
-exit 0
+# Return an error/success code (0/1)
+exit ${#errors[@]}
 
 
 # Date: 2017-06-21
